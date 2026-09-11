fff0757fb3d59ef48e8e82e0e2afd7bf9680c3cf
palmesjenny <c20246286.palmes@csav.edu.ph>
2026-09-11 10:21:01 +0800

    functions

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ let age = 60;
 const number =1234567809;
 const address = "gawahon";
 
-console.log("Name: s{myName}");
+console.log('Name: s{myName}');
+console.log('Name: ${myName}');
+console,log('Age: ${age}');
+consolke.log('Number: ${number}');
+console.log('Address: ${address}');
+
+function greet(name) {
+    return 'good morning,${name}';
+
+}
+console.log(greet(jenny)); // good morning, jenny
+
+function mdas(NUM1, NUM2) {
+    let m = NUM1 * NUM2;
+    let d = NUM1 / NUM2;
+    let a = NUM1 - NUM2;
+
+    return ('values: ${num1} and ${num2}, product ${m}, Quotient: ${d}, sum: ${a}, diff: ${s}');
+}
+
+console.log(mdas(5, 3));
+
+
+
+
 
 
 
